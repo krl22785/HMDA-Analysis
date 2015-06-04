94e8cf97670a8e8035f02433484d49d52d048ad5
krl22785 <krluna@gmail.com>
2015-06-04 16:43:06 -0400

    create directory for average mapreduce

---
+++ AverageMortgage/reduce.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys
+
+def parseInput(): 
+	for line in sys.stdin: 
+		yield line.strip('\n').split('\t')
+
+def reduce(): 
+	current_key = None 
+	current_count = 0 
+	current_sum = 0
+ 
+	for key, value in parseInput():
+	
+		value = eval(value)
+		
+		try:	
+			mortgage_sum = int(value[0])
+			mortgage_count = int(value[1])  		
+		except:
+			pass
+
+		if key == current_key:
+			current_sum += mortgage_sum 
+			current_count += mortgage_count		
+		else:
+			if current_key:
+				#avg_mortgage = float(current_sum)/float(current_count) 
+				#print avg_mortgage
+				#print "%s\t%.2f" % (current_key, avg_mortgage) 
+				output_value = (current_sum, current_count) 
+				print "%s\t%s" % (current_key, output_value) 
+
+			try: 
+				current_key = key 
+				current_sum = int(value[0])
+				current_count = int(value[1]) 
+			except:
+				pass 
+							
+if __name__=='__main__':
+	reduce()  
