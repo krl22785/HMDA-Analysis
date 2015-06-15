ac6f2d1fd24e2c1005c9f4150ff3df66308c0abb
krl22785 <krluna@gmail.com>
2015-06-14 21:20:22 -0400

    updated mapreduce for debtincome

--- DebtIncomeJava/DebtIncomeReducer.java
+++ DebtIncomeJava/DebtIncomeReducer.java
@@ -4,7 +4,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public static class DebtIncomeReducer extends <Text, Text, Text, Text> {
+public class DebtIncomeReducer 
+	extends Reducer<Text, Text, Text, Text> {
 	private Text result = new Text(); 
 	
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
@@ -18,10 +19,11 @@ public static class DebtIncomeReducer extends <Text, Text, Text, Text> {
 			String[] numbers = val.toString().split(":");
 			debtAmount += Double.parseDouble(numbers[0]);
 			incomeAmount += Double.parseDouble(numbers[1]); 
-			sumCount += Integer.parseInteger(numbers[2]); 
+			sumCount += Integer.parseInt(numbers[2]); 
 			}
 		
 		ratio = debtAmount/incomeAmount;		
-		result.set(key, ratio); 
+		result.set(Double.toString(ratio));
+		context.write(key, result); 
 		}	
 	}	
