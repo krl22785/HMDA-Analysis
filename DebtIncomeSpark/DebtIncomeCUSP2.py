a5535cab86b0d377d4cba47e489a1bc321824add
krl22785 <krluna@gmail.com>
2015-06-29 14:26:29 -0400

    updated my debt income spark

--- DebtIncomeSpark/DebtIncomeCUSP2.py
+++ DebtIncomeSpark/DebtIncomeCUSP2.py
@@ -7,24 +7,27 @@ def mapper(line):
 		return ('', (0.00, 1.00))
 
 	fields = eval("(%s)" % line)
-	#if fields[14] == 'One-to-four family dwelling (other than manufactured housing)' and \
-	#	fields[20] == 'Secured by a first lien' and \
-	#	fields[19] == 'Home purchase' and \
-	#	fields[46] == 'Loan originated' and \
-	#	fields[17] != '': 
+	if fields[14] == 'One-to-four family dwelling (other than manufactured housing)' and \
+		fields[20] == 'Secured by a first lien' and \
+		fields[19] == 'Home purchase' and \
+		fields[46] == 'Loan originated' and \
+		fields[17] != '': 
 			
-	key = (fields[9], fields[17], fields[26], fields[35])
-	amount = fields[6] 
-	income = fields[8]
+		key = (fields[9], fields[17], fields[26], fields[35])
+		amount = fields[6] 
+		income = fields[8]
 
-	if amount == '' or income == '':
-		amount = 0.00 
-		income = 1.00
-		value = (amount, income) 
-		return (key, value) 
-	else:
-		value = (float(amount), float(income))
-		return (key, value) 
+		if amount == '' or income == '':
+			amount = 0.00 
+			income = 1.00
+			value = (amount, income) 
+			return (key, value) 
+		else:
+			value = (float(amount), float(income))
+			return (key, value) 
+
+		
+	return ("none", (-1,-1))
 	
 if __name__=='__main__':
 	if len(sys.argv)!=3:
