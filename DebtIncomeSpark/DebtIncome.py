e85a7bff24d8d8f92e634c11d78aaa9b705b34d5
krl22785 <krluna@gmail.com>
2015-06-30 15:04:23 -0400

    updates to debt income spark script

--- DebtIncomeSpark/DebtIncome.py
+++ DebtIncomeSpark/DebtIncome.py
@@ -1,37 +1,38 @@
-"""DebtIncome.py"""
+import operator
+import sys
 from pyspark import SparkContext
-import StringIO
-import csv
-
-# the csv file needs to be processed to remove Non-ASCII characters as Spark will break if that is not done
-
-def loadRecord(line):
-        input = StringIO.StringIO(line) 
-        reader = csv.reader(input) 
-	return reader.next() 
 
 def mapper(line):
-	key = (line[9], line[17], line[26], line[35])
-	amount = line[6]
-	income = line[8]
-	if amount == '' or income == '': 
-		amount = 0.00
-		income = 1.00 
-		value = (amount, income) 
-		return (key, value) 
-	else:
-		value = (float(amount), float(income))	
-		return (key, value)  
+	if line.startswith('tract_to_msamd_income,rate_spread,population'):
+		return ('', (0.00, 1.00))
 
+	fields = eval("(%s)" % line)
+	if fields[14] == 'One-to-four family dwelling (other than manufactured housing)' and \
+		fields[20] == 'Secured by a first lien' and \
+		fields[19] == 'Home purchase' and \
+		fields[46] == 'Loan originated' and \
+		fields[17] != '': 
+			
+		key = (fields[9], fields[17], fields[26], fields[35])
+		amount = fields[6] 
+		income = fields[8]
 
-if __name__=='__main__': 
+		if amount == '' or income == '':
+			amount = 0.00 
+			income = 1.00
+			value = (amount, income) 
+			return (key, value) 
+		else:
+			value = (float(amount), float(income))
+			return (key, value) 
 	
-	file = "../../spark/hmda_sample3.csv"
-	sc = SparkContext("local", "Debt Income Ratio")
+	return ("none", (-1,-1))
+	
+if __name__=='__main__':
+	if len(sys.argv)!=3:
+		print "Usage: DebtIncome <input path> <output path>"
+        	sys.exit(-1)
 
-	input = sc.textFile(file).map(loadRecord).cache()
-	outputMap = input.map(mapper) 
-	
-	output = outputMap.reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) 	
-	
-	print output.mapValues(lambda x: x[0]/x[1]).collect()
+	sc = SparkContext(appName="Debt Income Ratio") # do not set 'master'
+    	output = sc.textFile(sys.argv[1]).map(mapper).reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1]))
+	output.mapValues(lambda x: x[0]/x[1]).saveAsTextFile(sys.argv[2])
