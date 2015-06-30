ab928b56de823f712a1662a3320e79e395ce78d9
krl22785 <krluna@gmail.com>
2015-06-30 15:34:37 -0400

    updated average mortgage status

--- AverageMortgageSpark/AverageMortgage.py
+++ AverageMortgageSpark/AverageMortgage.py
@@ -1,29 +1,37 @@
-"""SimpleApp.py"""
 from pyspark import SparkContext
-import StringIO
-import csv
-
-# the csv file needs to be processed to remove Non-ASCII characters as Spark will break if that is not done
-
-
-def loadRecord(line):
-	input = StringIO.StringIO(line) 
-	reader = csv.reader(input) 
-	return reader.next() 
+import sys 
 
 def mapper(line):
-	key = (line[9], line[17], line[26], line[34], line[35])
-	value = (float(line[6]), 1)
-	return (key, value) 
+	if line.startswith('tract_to_msamd_income'): 
+		return (('', '', '', '', ''), (0.00, 0.00)) 
+	
+	fields = eval("(%s)" % line)	
+	
+	if fields[14] == 'One-to-four family dwelling (other than manufactured housing)' and \
+		fields[20] == 'Secured by a first lien' and \
+		fields[19] == 'Home purchase' and \
+		fields[46] == 'Loan originated': 
+ 
+		key = (fields[9], fields[17], fields[26], fields[34], fields[35])	
+	
+		if fields[6] == '':
+			value = (0.00, 0.00) 
+			return (key, value) 
+
+		else: 
+			value = (float(fields[6]), 1)
+			return (key, value) 
+	else:
+		return ("none", (-1, -1))	
 
 if __name__=='__main__': 
-
-	file = "../../spark/hmda_sample3.csv"
-	sc = SparkContext("local", "Average Mortgage")
-
-	logData = sc.textFile(file).map(loadRecord).cache()
-	outputMap = logData.map(mapper) 
+	if len(sys.argv) != 3:
+		print "Usage: Average Mortgage <input path> <output path>" 
+		sys.exit(-1) 
 	
-	output = outputMap.reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) 	
-	print output.collect() 
+	sc = SparkContext(appName = "Average Mortgage") 
+	outputFile = sc.textFile(sys.argv[1]).map(mapper).reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) 
+	outputFile.saveAsTextFile(sys.argv[2])
+	
+
 	
