71ff3350ff94b36123a25d3b4f1094a11e66ab37
krl22785 <krluna@gmail.com>
2015-07-28 21:51:03 -0400

    updated spark job

--- ctClusters/msaCluster_more_features.py
+++ ctClusters/msaCluster_more_features.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext
+import sys 
 
 def find_owner_occupancy(att): 
 	if att == 'Owner-occupied as a principal dwelling':
@@ -17,7 +18,7 @@ def find_loan_purpose(att):
 		return 5 
 
 def find_gender(att):
-	if att == 'male':
+	if att == 'Male':
 		return 6
 	else:
 		return 7 
@@ -140,22 +141,14 @@ def reducer(a, b):
 
 if __name__=='__main__': 
 
-	file = "../files/hmda_sample.csv"
-	sc = SparkContext("local", "MSA Cluster")
+	if len(sys.argv) != 3:
+		print "Usage: msaCluster <input path> <output path>" 
+		sys.exit(-1)
+	
+	sc = SparkContext(appName = "MSA Features")
+	msa_data = sc.textFile(sys.argv[1]).map(mapper)
+	msa_data_grouped = msa_data.reduceByKey(reducer).saveAsTextFile(sys.argv[2]) 
 
-	data = sc.textFile(file).map(mapper)	
-	data_output = data.reduceByKey(reducer) 	
-	
+	#file = "../files/hmda_sample.csv"
+	#sc = SparkContext("local", "MSA Cluster")
 
-	#data_output = data.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1], x[2] + y[2], x[3] + y[3]))
-
-	for i, j in enumerate(data_output.collect()): print i, j 
-	
-	#for i in logData.collect():
-	#	print i 
-	
-	#outputMap = logData.map(mapper) 
-	
-	#output = outputMap.reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) 	
-	#print output.collect() 
-	
